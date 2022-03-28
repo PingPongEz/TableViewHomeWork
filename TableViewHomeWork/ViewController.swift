f434b48f18795fdbc71925c15ebe7f3eaa92848b
Sergey <kopgamerk@gmail.com>
2022-03-28 08:28:11 +0300

    Initial Commit

---
+++ TableViewHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewHomeWork
+//
+//  Created by u on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
